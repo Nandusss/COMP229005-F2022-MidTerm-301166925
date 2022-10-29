bba5c2c13ba4ab6fc6171d91039d9dec0c604b8a
Nandagopan Dilip <113753010+Nandusss@users.noreply.github.com>
2022-10-29 07:36:49 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 // Do not expose your credentials in your code.
-let atlasDB = "mongodb+srv://<username>:<passoword>@<cluster>/todo?retryWrites=true&w=majority";
+let atlasDB = "mongodb+srv://somebody:2ZJbMoJ5xcOW7Vvj@cluster0.7w7pf0q.mongodb.net/?retryWrites=true&w=majority";
 
 // Database setup
 let mongoose = require('mongoose');
